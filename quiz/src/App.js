d79c42c19c813f753fb70da5713725eacf5b9436
Mike <mr.m.botkin@gmail.com>
2020-04-11 18:16:23 +0300

    QZ ADD "Route" and restructure App

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Layout from "./hoc/Layout/layout";
+import { Route, Switch } from "react-router-dom";
 import Quiz from "./containers/quiz/quiz";
+import Auth from "./containers/Auth/auth";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import QuizList from "./containers/QuizList/QuizList";
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/quiz-creator" component={QuizCreator} />
+        <Route path="/quiz/:id" component={Quiz} />
+        <Route path="/" component={QuizList} />
+      </Switch>
     </Layout>
   );
 }

535f594a54163d98b51c9aa1c7930e52691022ba
Mike <mr.m.botkin@gmail.com>
2020-04-18 10:17:38 +0300

    QZ ADD Auth component. Weak CSS

--- quiz/src/containers/Auth/auth.js
+++ quiz/src/containers/Auth/auth.js
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
+import classes from "./Auth.module.css";
+import Button from "../../components/UI/button/button";
 
 export default class Auth extends Component {
+  loginHandler = () => {
+    console.log("click-click");
+  };
+
+  regHandler = () => {
+    console.log("reg");
+  };
+
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   render() {
-    return <div>Auth</div>;
+    return (
+      <div className={classes.Auth}>
+        <div className={classes.AuthWrapper}>
+          <h1>Authorization</h1>
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <input type="text" />
+            <input type="text" />
+            <Button type="success" onClick={this.loginHandler}>
+              Log In
+            </Button>
+            <Button type="primary" onClick={this.regHandler}>
+              Registration
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }

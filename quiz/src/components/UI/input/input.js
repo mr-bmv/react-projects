8546f574059de75b8980ddf0e907acf137cf6328
Mike <mr.m.botkin@gmail.com>
2020-04-21 18:35:19 +0300

    QZ Add base validation into input

--- quiz/src/components/UI/input/input.js
+++ quiz/src/components/UI/input/input.js
@@ -1,15 +1,35 @@
 import React from "react";
 import classes from "./input.module.css";
 
-const Input = ({ type, label, value, onChange, errorMessage }) => {
+const isInvalid = (valid, touched, shouldValidate) => {
+  return !valid && shouldValidate && touched;
+};
+
+const Input = ({
+  type,
+  label,
+  value,
+  onChange,
+  errorMessage,
+  valid,
+  touched,
+  shouldValidate,
+}) => {
   const inputType = type || "text";
   const cls = [classes.Input];
   const htmlFor = `${inputType}-${Math.random()}`;
+
+  if (isInvalid({ valid, touched, shouldValidate })) {
+    cls.push(classes.invalid);
+  }
+
   return (
     <div className={cls.join(" ")}>
       <label htmlFor={htmlFor}>{label}</label>
       <input type={inputType} id={htmlFor} value={value} onChange={onChange} />
-      <span>{errorMessage}</span>
+      {isInvalid({ valid, touched, shouldValidate }) ? (
+        <span>{errorMessage || "Please, fill in the form correctly"}</span>
+      ) : null}
     </div>
   );
 };

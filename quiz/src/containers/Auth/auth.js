c3c1a5abbaba634f4efb728f191734ff13f6f391
Mike <mr.m.botkin@gmail.com>
2020-04-21 18:34:50 +0300

    QZ Render Input in Auth

--- quiz/src/containers/Auth/auth.js
+++ quiz/src/containers/Auth/auth.js
@@ -4,6 +4,34 @@ import Button from "../../components/UI/button/button";
 import Input from "../../components/UI/input/input";
 
 export default class Auth extends Component {
+  state = {
+    formControls: {
+      email: {
+        value: "",
+        type: "email",
+        label: "Email",
+        errorMessage: "Incorrect E-mail",
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: "",
+        type: "password",
+        label: "Password",
+        errorMessage: "Incorrect Password",
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+      },
+    },
+  };
   loginHandler = () => {
     console.log("click-click");
   };
@@ -16,14 +44,38 @@ export default class Auth extends Component {
     event.preventDefault();
   };
 
+  onChangeHandler = (event, credential) => {
+    console.log(event.target.value, "-", credential);
+  };
+
+  renderInputs = () => {
+    return Object.keys(this.state.formControls).map((credential, index) => {
+      const control = this.state.formControls[credential];
+      return (
+        <Input
+          key={credential + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={(event) => this.onChangeHandler(event, credential)}
+        />
+      );
+    });
+  };
+
   render() {
     return (
       <div className={classes.Auth}>
         <div className={classes.AuthWrapper}>
           <h1>Authorization</h1>
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <Input label="E-mail" />
-            <Input label="Password" errorMessage="TEST" />
+            {/* <Input label="E-mail" />
+            <Input label="Password" errorMessage="TEST" /> */}
+            {this.renderInputs()}
             <Button type="success" onClick={this.loginHandler}>
               Log In
             </Button>

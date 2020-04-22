f20fb22ad56b5e6cdd51efb2e681d13f5f2f85d5
Mike <mr.m.botkin@gmail.com>
2020-04-22 22:56:44 +0300

    QZ Add QuizCreztr component + CSS

--- quiz/src/containers/QuizCreator/QuizCreator.js
+++ quiz/src/containers/QuizCreator/QuizCreator.js
@@ -1,11 +1,47 @@
-import React from "react";
-import { Component } from "react";
+import React, { Component } from "react";
+import Button from "./../../components/UI/button/button";
+import Input from "./../../components/UI/input/input";
+
+import classes from "./QuizCreator.module.css";
 
 export default class QuizCreator extends Component {
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  addQuestionHandler = () => {
+    console.log("Add new Test");
+  };
+
+  createQuizHandler = () => {
+    console.log("Submit Test");
+  };
+
   render() {
     return (
-      <div>
-        <h1>Quiz Creator</h1>
+      <div className={classes.QuizCreator}>
+        <div>
+          <h1>Create New Test</h1>
+
+          <form onSubmit={this.submitHandler}>
+            <Input type="text" />
+            <hr />
+            <Input type="text" />
+            <Input type="text" />
+            <Input type="text" />
+            <Input type="text" />
+
+            <select></select>
+
+            <Button type="primary" onClick={this.addQuestionHandler}>
+              Add new Question
+            </Button>
+
+            <Button type="success" onClick={this.createQuizHandler}>
+              Submit Test
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }

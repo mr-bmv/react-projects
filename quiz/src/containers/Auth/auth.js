e3a47de7e7292951dcbf8c73bf1897b15fb5f7fb
Mike <mr.m.botkin@gmail.com>
2020-04-22 13:53:01 +0300

    QZ Clean Up Code

--- quiz/src/containers/Auth/auth.js
+++ quiz/src/containers/Auth/auth.js
@@ -4,7 +4,7 @@ import Button from "../../components/UI/button/button";
 import Input from "../../components/UI/input/input";
 
 const validateEmail = (email) => {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 };
 

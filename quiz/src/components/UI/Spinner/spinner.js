94d37948fd4b12da1c8743d9b6491f719a72c45c
Mike <mr.m.botkin@gmail.com>
2020-04-29 10:53:02 +0300

    QZ Add Spinner

---
+++ quiz/src/components/UI/Spinner/spinner.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="center">
+      <div className="loadingio-spinner-gear-4t5fmwmfu47">
+        <div className="ldio-fv6kig07dqq">
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;

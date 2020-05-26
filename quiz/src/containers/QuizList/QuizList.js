24e2d5e1dd281b6d883a8ca916ba45807efc3333
Mike <mr.m.botkin@gmail.com>
2020-05-26 11:01:38 +0300

    QZ Generate URL for each test. Add spinner

--- quiz/src/containers/QuizList/QuizList.js
+++ quiz/src/containers/QuizList/QuizList.js
@@ -2,24 +2,40 @@ import React, { Component } from "react";
 import classes from "./QuizList.module.css";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
+import Spinner from "../../components/UI/Spinner/spinner";
 
 export default class QuizList extends Component {
-  tests = [1, 2, 3];
+  state = {
+    tests: [],
+    loading: true,
+  };
 
   renderList = () => {
-    return this.tests.map((quiz, index) => {
+    return this.state.tests.map(({ id, name }) => {
       return (
-        <li key={index}>
-          <NavLink to={"/quiz/" + quiz}>Quiz №{quiz}</NavLink>
+        <li key={id}>
+          <NavLink to={"/quiz/" + id}>{name}</NavLink>
         </li>
       );
     });
   };
 
-  componentDidMount = () => {
-    axios
-      .get("https://react-quiz-37a08.firebaseio.com/quiz.json")
-      .then((res) => console.log(res));
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get(
+        "https://react-quiz-37a08.firebaseio.com/tests.json"
+      );
+      const test = [];
+      Object.keys(response.data).forEach((id, index) => {
+        test.push({
+          id,
+          name: `Test ${index + 1}`,
+        });
+      });
+      this.setState({ tests: test, loading: false });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   render() {
@@ -29,7 +45,7 @@ export default class QuizList extends Component {
           <h1>Choose your Quiz</h1>
           <div className={classes.QuizListInside}>
             <span>List of Tests:</span>
-            <ul>{this.renderList()}</ul>
+            {this.state.loading ? <Spinner /> : <ul>{this.renderList()}</ul>}
           </div>
         </div>
       </div>

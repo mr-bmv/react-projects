a35c44d40a39a09f8dd50ba532816be8d69f9967
mr-bmv <mr.m.botkin@gmail.com>
2020-07-14 16:17:38 +0300

    RX Add Middleware for counter

--- counter/src/index.js
+++ counter/src/index.js
@@ -4,11 +4,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./redux/rootReducer";
 import { Provider } from "react-redux";
 
-const store = createStore(rootReducer);
+const loggerMiddleware = store => next => action => {
+  const result = next(action)
+  console.log('Middleware', store.getState());
+  return result
+}
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 const app = (
   <Provider store={store}>

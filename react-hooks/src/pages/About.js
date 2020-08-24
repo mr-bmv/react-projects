684157e7c66a4962e69dc3e58f1a08c56b75e5cb
mr-bmv <mr.m.botkin@gmail.com>
2020-08-24 14:28:08 +0300

    GIT Init new projec react-hooks

---
+++ react-hooks/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const About = () => {
+    return (
+        <div className="jumbotron">
+            <div className="container">
+                <h1 className="display-4">Information</h1>
+                <p className="lead">Version:<strong>1.0.0</strong></p>
+                <p className="lead">Application was created by <strong>Botkin Mikhail</strong></p>
+            </div>
+        </div>
+    )
+}

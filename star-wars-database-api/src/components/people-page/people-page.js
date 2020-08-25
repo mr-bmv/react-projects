2dc5b0c253108cb2c5880b10126c137ab06b7224
mr-bmv <mr.m.botkin@gmail.com>
2020-08-25 11:56:44 +0300

    SW Remov ErrorBoundary/ContentRow from people-page

--- star-wars-database-api/src/components/people-page/people-page.js
+++ star-wars-database-api/src/components/people-page/people-page.js
@@ -3,44 +3,9 @@ import "./people-page.css";
 
 import ItemList from "../item-list";
 import ItemDetails from "../item-details";
-import ErrorIndicator from "../error-indicator";
 import SwapiService from "../../services/swapi-services";
-
-// container
-// create container when some of layout would be re-used
-// each container should be React element
-// we can take from `props` or destructuring it in parameters
-
-const PeopleContainer = ({ leftSide, rightSide }) => {
-  return (
-    <div className="row mb2">
-      <div className="col-md-6">
-        {leftSide}
-      </div>
-      <div className="col-md-6">
-        {rightSide}
-      </div>
-    </div>
-  )
-}
-
-class ErrorBoundary extends Component {
-  state = {
-    hasError: false
-  }
-
-  componentDidCatch = () => {
-    this.setState({ hasError: true });
-  };
-
-  render() {
-    if (this.state.hasError) {
-      return <ErrorIndicator />;
-    }
-    return this.props.children
-  }
-}
-
+import ContentRow from "../../container/ContentRow";
+import ErrorBoundary from "../../hoc/ErrorBoundary";
 
 export default class PeoplePage extends Component {
   swapiService = new SwapiService();
@@ -64,7 +29,7 @@ export default class PeoplePage extends Component {
       </ErrorBoundary >
 
     return (
-      <PeopleContainer leftSide={itemList} rightSide={itemDetails} />
+      <ContentRow leftSide={itemList} rightSide={itemDetails} />
     );
   }
 }

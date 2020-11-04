11499045ec0687ccc6e8715581ff65ae820398d7
mr-bmv <mr.m.botkin@gmail.com>
2020-11-04 16:29:29 +0300

    SW Page for each Starship

--- star-wars-database-api/src/components/app/app.js
+++ star-wars-database-api/src/components/app/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // components
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorBoundary from '../error-boundary';
+import PeoplePage from '../pages/people-page';
+import PlanetPage from '../pages/planet-page/planet-page';
+import StarshipPage from '../pages/starship-page/starship-page';
 
 // services
 import SwapiService from '../../services/swapi-service';
@@ -12,10 +16,9 @@ import DummySwapiService from '../../services/dummy-swapi-service';
 // context
 import { SwapiServiceProvider } from '../swapi-service-context';
 
+// style
 import './app.css';
-import PeoplePage from '../pages/people-page';
-import PlanetPage from '../pages/planet-page/planet-page';
-import StarshipPage from '../pages/starship-page/starship-page';
+import { StarshipDetails } from '../sw-components';
 
 export default class App extends Component {
 
@@ -38,14 +41,23 @@ export default class App extends Component {
     return (
       <ErrorBoundary>
         <SwapiServiceProvider value={this.state.swapiService} >
-          <div className="stardb-app">
-            <Header onServiceChange={this.onServiceChange} />
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
+              <RandomPlanet />
 
-            <RandomPlanet />
-            <PeoplePage />
-            <PlanetPage />
-            <StarshipPage />
-          </div>
+              <Route path="/" exact render={() => <h2>Wellcome to Star DB</h2>} />
+              <Route path='/people' component={PeoplePage} />
+              <Route path='/planets' component={PlanetPage} />
+              <Route path='/starships' exact component={StarshipPage} />
+              <Route path='/starships/:id'
+                render={({ match }) => {
+                  console.log(match.params.id)
+                  return <StarshipDetails itemId={match.params.id} />
+                }} />
+
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundary>
     );

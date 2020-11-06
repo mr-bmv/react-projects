621cb13d8bb412d883d529e00d3bd54ef9d52a7e
mr-bmv <mr.m.botkin@gmail.com>
2020-11-06 16:22:34 +0300

    BookstoreContext was added

--- re-store/src/components/App/App.jsx
+++ re-store/src/components/App/App.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import BookstoreService from '../../services/bookstore-service'
+import { BookServiceProvider } from '../bookstore-service-context/bookstore-service-context'
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
-import ErrorIndicator from '../ErrorIndicator/ErrorIndicator'
 import Spinner from '../Spinner/Spinner'
 
 export default class App extends Component {
+
+    state = {
+        bookstoreService: new BookstoreService()
+    }
+
+
     render() {
         return (
             <ErrorBoundary>
-                <Spinner />
+                <BookServiceProvider value={this.state.bookstoreService} >
+                    <Spinner />
+                </BookServiceProvider>
             </ErrorBoundary>
         )
     }

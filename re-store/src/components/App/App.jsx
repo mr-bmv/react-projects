cd69c16f6573ae1e188448da1d86d4120b16664a
mr-bmv <mr.m.botkin@gmail.com>
2020-11-06 21:28:47 +0300

    Skeleton for application was created

--- re-store/src/components/App/App.jsx
+++ re-store/src/components/App/App.jsx
@@ -1,23 +1,9 @@
-import React, { Component } from 'react'
-import BookstoreService from '../../services/bookstore-service'
-import { BookServiceProvider } from '../bookstore-service-context/bookstore-service-context'
-import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
-import Spinner from '../Spinner/Spinner'
+import React from 'react';
+import withBookstoreService from '../HOC/withBookstoreService';
 
-export default class App extends Component {
-
-    state = {
-        bookstoreService: new BookstoreService()
-    }
-
-
-    render() {
-        return (
-            <ErrorBoundary>
-                <BookServiceProvider value={this.state.bookstoreService} >
-                    <Spinner />
-                </BookServiceProvider>
-            </ErrorBoundary>
-        )
-    }
+const App = ({ bookstoreService }) => {
+    console.log(bookstoreService.getBooks())
+    return (<div>App</div>);
 };
+
+export default withBookstoreService()(App);
--- re-store/src/index.js
+++ re-store/src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
+import { BookServiceProvider } from './components/bookstore-service-context/bookstore-service-context';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import BookstoreService from './services/bookstore-service';
+import store from './store'
 
-ReactDOM.render(<App />,
+const bookstoreService = new BookstoreService();
+
+ReactDOM.render(
+    // To connect our redux with react app
+    // Our components will have access to date which we have in store
+    <Provider store={store}>
+        {/* ErrorBoundary should be inside of Provider because it could
+        need to have access to some date from Provider. F.e. to the language for 
+        show correct message */}
+        <ErrorBoundary store={store}>
+            {/*  all components from App will have access to our service and
+            it means to data from this service */}
+            <BookServiceProvider value={bookstoreService} >
+                {/* all components from our App will have access to Router */}
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </BookServiceProvider>
+        </ErrorBoundary>
+    </Provider>
+    ,
     document.getElementById('root'));

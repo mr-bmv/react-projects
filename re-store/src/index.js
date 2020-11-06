fce39ed629acadd3d33d369080ff10ec5abcfd8f
mr-bmv <mr.m.botkin@gmail.com>
2020-11-06 15:55:07 +0300

    index.js was formed

--- re-store/src/index.js
+++ re-store/src/index.js
@@ -1,1 +1,6 @@
-console.log('Start new Project')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));

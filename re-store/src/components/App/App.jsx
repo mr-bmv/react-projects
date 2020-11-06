6973270ef2188fa9430147fce5a205561b89c5f5
mr-bmv <mr.m.botkin@gmail.com>
2020-11-06 16:13:42 +0300

    ErrorBoundary was added

--- re-store/src/components/App/App.jsx
+++ re-store/src/components/App/App.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
 import ErrorIndicator from '../ErrorIndicator/ErrorIndicator'
 import Spinner from '../Spinner/Spinner'
 
 export default class App extends Component {
     render() {
         return (
-            <div>
+            <ErrorBoundary>
                 <Spinner />
-                <ErrorIndicator />
-            </div>
-
+            </ErrorBoundary>
         )
     }
 };

024e434820513a53321eefe83e5d2df53cd48e53
mr-bmv <mr.m.botkin@gmail.com>
2020-11-10 16:46:28 +0300

    Code Refactoring Add actionType folder

--- re-store/src/reducers/index.js
+++ re-store/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { FETCH_BOOKS_FAILURE, FETCH_BOOKS_REQUEST, FETCH_BOOKS_SUCCESS } from "../actions/actionTypes";
+
 const initialState = {
   books: [],
   loading: true,
@@ -8,19 +10,19 @@ const reducer = (state = initialState, action) => {
 
   switch (action.type) {
     // to change loading in true, when we jump from page to page
-    case 'FETCH_BOOKS_REQUEST':
+    case FETCH_BOOKS_REQUEST:
       return {
         books: [],
         loading: true,
         error: false
       }
-    case 'FETCH_BOOKS_SUCCESS':
+    case FETCH_BOOKS_SUCCESS:
       return {
         books: action.payload,
         loading: false,
         error: false
       };
-    case 'FETCH_BOOKS_FAILURE':
+    case FETCH_BOOKS_FAILURE:
       return {
         books: [],
         loading: false,
